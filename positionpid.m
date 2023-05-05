f2afcc36381035e96aff075e7c3239b1d18893d8
Liuck27 <luca.secchieri@gmail.com>
2023-05-05 17:05:54 +0200

    Aggiornato il pid con il processo sysP giusto

--- positionpid.m
+++ positionpid.m
@@ -6,7 +6,9 @@ alpha = 4;
 
 %% Controller tuning
 
-sysP = tf(Km/gbox.N, [Tm 1 0]);
+s = tf('s');
+sysDprime = s*(s*Jeq+Beq)*(mld.Jb*s + mld.Bb)+ mld.k*(s*(Jeq+mld.Jb/gbox.N^2)+(Beq+mld.Bb/gbox.N^2));
+sysP = (1/(gbox.N*s))*(drv.dcgain*mot.Kt)*(mld.Jb*s^2 + mld.Bb*s + mld.k)/(Req*sysDprime+mot.Kt*mot.Ke*(mld.Jb*s^2 + mld.Jb*s + mld.k));
 
 
 [PID, info] = pidtune(sysP, 'PIDF', omega_gc);
